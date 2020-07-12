a3327b13d3f34ecc8e574411533b31848f3460c3
Alex Kesling <alex@kesling.co>
2020-07-12 15:01:38 -0400

    Create a basic wheel written in Rust

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+
+if [ "$(uname)" = "Darwin" ]; then
+    pip install "$(ls sidecar/artifacts/wheels/ | grep mac | tail -n 1)"
+else
+    pip install "$(ls sidecar/artifacts/wheels/ | grep linux | tail -n 1)"
+fi
