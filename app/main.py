579cf03d3061c315eba2565f09fb18a90c9aba9e
Alex Kesling <alex@kesling.co>
2020-07-17 12:08:39 -0400

    SPAM SPAM SPAM SPAM!

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
 import requests
 import sys
 
-
 def main():
     server_url = sys.argv[1]
     player_key = sys.argv[2]
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
 
-    res = requests.post(server_url, data=player_key)
-    if res.status_code != 200:
-        print('Unexpected server response:')
-        print('HTTP code:', res.status_code)
-        print('Response body:', res.text)
-        exit(2)
-    print('Server response:', res.text)
-
+    for i in range(100000):
+        res = requests.post(server_url, data=str(i))
+        if res.status_code != 200:
+            print('Unexpected server response:')
+            print('HTTP code:', res.status_code)
+            print('Response body:', res.text)
+            exit(2)
+        print('Server response:', res.text)
 
 if __name__ == '__main__':
     main()
