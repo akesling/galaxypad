259acada924a956a5755390b9c76759ef3e7bf0a
Alex Kesling <alex@kesling.co>
2020-07-17 14:08:41 -0400

    Fix int_to_grid and add grid_to_int

--- lib/ops.py
+++ lib/ops.py
@@ -12,9 +12,32 @@ def int_to_grid(number):
         row[0] = 1
     grid[0][0] = 0
 
-    for i, char in enumerate(bin_string):
+    for i, char in enumerate(reversed(list(bin_string))):
         row = (i // size) + 1
         col = (i % size) + 1
         grid[row][col] = int(char)
 
     return grid
+
+def grid_to_int(grid):
+    height = len(grid)
+    width = len(grid[0])
+    is_negative = height > width
+
+    bin_list = []
+    for i in range(1, height-(is_negative*1)):
+        row = grid[i]
+        print(row)
+        for value in row[1:]:
+            bin_list.append(str(value))
+
+    print(bin_list)
+    bin_string = ''.join(reversed(bin_list))
+    print(bin_string)
+    number = int(bin_string, 2)
+    print(number)
+
+    if is_negative:
+        return -1 * number
+    else:
+        return number
