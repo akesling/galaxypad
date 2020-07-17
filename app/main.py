5cda456dda8111a732a8baf3a5c8301130236167
A Ray <a@machinaut.com>
2020-07-17 15:52:17 -0700

    more messages

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 import requests
 import sys
 
+def reg(server_url):  # Request get
+    print("Getting", server_url)
+    res = requests.get(server_url)
+    if res.status_code != 200:
+        print('Unexpected server response from URL "%s":' % server_url)
+        print('HTTP code:', res.status_code)
+        print('Response body:', res.text)
+        # exit(2)
+    else:
+        print('Server response:', res.text)
+    print('whole res')
+    print(res)
+    print(repr(res))
+    return res
 
 def req(server_url, data):
     print('Sending "%s"' % data)
@@ -22,16 +36,24 @@ def main():
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
 
     # Announce
-    res = req(server_url, player_key)
-    print(res)
-    print(repr(res))
+    print("announce")
+    req(server_url, player_key)
 
     # Make request to aliens API
     alien_url = server_url + '/aliens/send'
     print('aliens send url', alien_url)
-    res = req(alien_url, '"1101000"')
-    print(res)
-    print(repr(res))
+    print('sending ""')
+    req(alien_url, "")
+    print('sending "1101000"')
+    req(alien_url, "1101000")
+    print('sending "010"')
+    req(alien_url, "010")
+    print('sending "0"')
+    req(alien_url, "0")
+    print('sending "a"')
+    req(alien_url, "a")
+    print('sending "hello"')
+    req(alien_url, "hello")
 
 if __name__ == '__main__':
     main()
