cdf8d344d237e18516d7c1f9ed5c68783e8d35c2
Alex Kesling <alex@kesling.co>
2020-07-17 12:24:49 -0400

    Prefix SPAM with player key

--- app/main.py
+++ app/main.py
@@ -1,19 +1,24 @@
 import requests
 import sys
 
+def req(data):
+    res = requests.post(server_url, data=str(i))
+    if res.status_code != 200:
+        print('Unexpected server response:')
+        print('HTTP code:', res.status_code)
+        print('Response body:', res.text)
+        exit(2)
+    print('Server response:', res.text)
+    return res
+
 def main():
     server_url = sys.argv[1]
     player_key = sys.argv[2]
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
 
+    req(player_key)
     for i in range(100000):
-        res = requests.post(server_url, data=str(i))
-        if res.status_code != 200:
-            print('Unexpected server response:')
-            print('HTTP code:', res.status_code)
-            print('Response body:', res.text)
-            exit(2)
-        print('Server response:', res.text)
+        res = req(str(i))
 
 if __name__ == '__main__':
     main()
