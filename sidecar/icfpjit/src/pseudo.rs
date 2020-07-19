31f25d1086ea1171ca804da9c200990926c60b68
Alex Kesling <alex@kesling.co>
2020-07-19 19:19:42 -0400

    cargo fmt

--- sidecar/icfpjit/src/pseudo.rs
+++ sidecar/icfpjit/src/pseudo.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::Debug;
 use std::ptr;
+use std::rc::Rc;
 
 // See video course https://icfpcontest2020.github.io/#/post/2054
 
@@ -39,7 +39,10 @@ impl Expr for Atom {
     }
 
     fn set_evaluated(&mut self, expr: ExprRef) -> Result<(), String> {
-        Err(format!("Attempted to set Atom to evaluated with value: {:?}", expr))
+        Err(format!(
+            "Attempted to set Atom to evaluated with value: {:?}",
+            expr
+        ))
     }
 }
 
@@ -61,7 +64,10 @@ impl Expr for &Atom {
     }
 
     fn set_evaluated(&mut self, expr: ExprRef) -> Result<(), String> {
-        Err(format!("Attempted to set Atom to evaluated with value: {:?}", expr))
+        Err(format!(
+            "Attempted to set Atom to evaluated with value: {:?}",
+            expr
+        ))
     }
 }
 
@@ -130,11 +136,19 @@ fn parse_functions(script_string: &str) -> HashMap<String, ExprRef> {
     panic!("Parse functions is not yet implemented");
 }
 
-fn interact(state: ExprRef, event: ExprRef, functions: &HashMap<String, ExprRef>, constants: &HashMap<String, ExprRef>) -> (ExprRef, ExprRef) {
+fn interact(
+    state: ExprRef,
+    event: ExprRef,
+    functions: &HashMap<String, ExprRef>,
+    constants: &HashMap<String, ExprRef>,
+) -> (ExprRef, ExprRef) {
     // See https://message-from-space.readthedocs.io/en/latest/message38.html
-    let expr: ExprRef = Rc::new(RefCell::new(Ap{
-        func: Rc::new(RefCell::new(Ap{
-            func: Rc::new(RefCell::new(Atom{name: "galaxy".to_owned(), ..Default::default()})),
+    let expr: ExprRef = Rc::new(RefCell::new(Ap {
+        func: Rc::new(RefCell::new(Ap {
+            func: Rc::new(RefCell::new(Atom {
+                name: "galaxy".to_owned(),
+                ..Default::default()
+            })),
             arg: state.clone(),
             _evaluated: None,
         })),
@@ -145,7 +159,10 @@ fn interact(state: ExprRef, event: ExprRef, functions: &HashMap<String, ExprRef>
     // Note: res will be modulatable here (consists of cons, nil and numbers only)
     let items = get_list_items_from_expr(res).unwrap();
     if items.len() < 3 {
-        panic!("List was of unexpected length {}, expected 3 items", items.len());
+        panic!(
+            "List was of unexpected length {}, expected 3 items",
+            items.len()
+        );
     }
     let (flag, newState, data) = (items[0].clone(), items[1].clone(), items[2].clone());
     if (as_num(flag) == 0) {
@@ -166,11 +183,15 @@ fn get_list_items_from_expr(expr: ExprRef) -> Result<Vec<ExprRef>, String> {
     panic!("Eval is not yet implemented");
 }
 
-fn eval(expr: ExprRef, functions: &HashMap<String, ExprRef>, constants: &HashMap<String, ExprRef>) -> Result<ExprRef, String> {
+fn eval(
+    expr: ExprRef,
+    functions: &HashMap<String, ExprRef>,
+    constants: &HashMap<String, ExprRef>,
+) -> Result<ExprRef, String> {
     match expr.borrow().evaluated() {
         Some(expr) => {
             panic!("Eval is not yet implemented");
-        },
+        }
         None => {
             let mut current_expr = expr.clone();
             loop {
@@ -186,7 +207,11 @@ fn eval(expr: ExprRef, functions: &HashMap<String, ExprRef>, constants: &HashMap
     }
 }
 
-fn try_eval(expr: ExprRef, functions: &HashMap<String, ExprRef>, constants: &HashMap<String, ExprRef>) -> Result<ExprRef, String> {
+fn try_eval(
+    expr: ExprRef,
+    functions: &HashMap<String, ExprRef>,
+    constants: &HashMap<String, ExprRef>,
+) -> Result<ExprRef, String> {
     match expr.borrow().evaluated() {
         Some(x) => {
             return Ok(x.clone());
@@ -202,44 +227,44 @@ fn try_eval(expr: ExprRef, functions: &HashMap<String, ExprRef>, constants: &Has
                 if let Some(name) = expr.borrow().name().as_ref() {
                     match *name {
                         "neg" => {
-                            return Ok(Rc::new(RefCell::new(Atom{
-                                name: (-as_num(eval(x, functions, constants)?)).to_string()
+                            return Ok(Rc::new(RefCell::new(Atom {
+                                name: (-as_num(eval(x, functions, constants)?)).to_string(),
                             })));
-                        },
+                        }
                         "i" => {
                             return Ok(x);
-                        },
-//                    if (fun.Name == "nil") return t
-//                        "nil" {
-//                            return Ok(t);
-//                        }
-//                    if (fun.Name == "isnil") return Ap(x, Ap(t, Ap(t, f)))
-//                    if (fun.Name == "car") return Ap(x, t)
-//                    if (fun.Name == "cdr") return Ap(x, f)
+                        }
+                        //                    if (fun.Name == "nil") return t
+                        //                        "nil" {
+                        //                            return Ok(t);
+                        //                        }
+                        //                    if (fun.Name == "isnil") return Ap(x, Ap(t, Ap(t, f)))
+                        //                    if (fun.Name == "car") return Ap(x, t)
+                        //                    if (fun.Name == "cdr") return Ap(x, f)
                         _ => (),
                     }
                 }
-        //        if (fun is Ap)
-        //            Expr fun2 = eval(fun.Fun)
-        //            Expr y = fun.Arg
-        //            if (fun2 is Atom)
-        //                if (fun2.Name == "t") return y
-        //                if (fun2.Name == "f") return x
-        //                if (fun2.Name == "add") return Atom(asNum(eval(x)) + asNum(eval(y)))
-        //                if (fun2.Name == "mul") return Atom(asNum(eval(x)) * asNum(eval(y)))
-        //                if (fun2.Name == "div") return Atom(asNum(eval(y)) / asNum(eval(x)))
-        //                if (fun2.Name == "lt") return asNum(eval(y)) < asNum(eval(x)) ? t : f
-        //                if (fun2.Name == "eq") return asNum(eval(x)) == asNum(eval(y)) ? t : f
-        //                if (fun2.Name == "cons") return evalCons(y, x)
-        //            if (fun2 is Ap)
-        //                Expr fun3 = eval(fun2.Fun)
-        //                Expr z = fun2.Arg
-        //                if (fun3 is Atom)
-        //                    if (fun3.Name == "s") return Ap(Ap(z, x), Ap(y, x))
-        //                    if (fun3.Name == "c") return Ap(Ap(z, x), y)
-        //                    if (fun3.Name == "b") return Ap(z, Ap(y, x))
-        //                    if (fun3.Name == "cons") return Ap(Ap(x, z), y)
-        //    return expr
+                //        if (fun is Ap)
+                //            Expr fun2 = eval(fun.Fun)
+                //            Expr y = fun.Arg
+                //            if (fun2 is Atom)
+                //                if (fun2.Name == "t") return y
+                //                if (fun2.Name == "f") return x
+                //                if (fun2.Name == "add") return Atom(asNum(eval(x)) + asNum(eval(y)))
+                //                if (fun2.Name == "mul") return Atom(asNum(eval(x)) * asNum(eval(y)))
+                //                if (fun2.Name == "div") return Atom(asNum(eval(y)) / asNum(eval(x)))
+                //                if (fun2.Name == "lt") return asNum(eval(y)) < asNum(eval(x)) ? t : f
+                //                if (fun2.Name == "eq") return asNum(eval(x)) == asNum(eval(y)) ? t : f
+                //                if (fun2.Name == "cons") return evalCons(y, x)
+                //            if (fun2 is Ap)
+                //                Expr fun3 = eval(fun2.Fun)
+                //                Expr z = fun2.Arg
+                //                if (fun3 is Atom)
+                //                    if (fun3.Name == "s") return Ap(Ap(z, x), Ap(y, x))
+                //                    if (fun3.Name == "c") return Ap(Ap(z, x), y)
+                //                    if (fun3.Name == "b") return Ap(z, Ap(y, x))
+                //                    if (fun3.Name == "cons") return Ap(Ap(x, z), y)
+                //    return expr
             }
         }
     }
@@ -256,26 +281,51 @@ fn request_click_from_user() -> Point {
 }
 
 fn main() {
-
     let functions = parse_functions("DUMMY VALUE");
     let mut constants: HashMap<String, ExprRef> = HashMap::new();
-    constants.insert("cons".to_owned(), Rc::new(RefCell::new(Atom{name: "cons".to_owned()})));
-    constants.insert("t".to_owned(), Rc::new(RefCell::new(Atom{name: "t".to_owned()})));
-    constants.insert("f".to_owned(), Rc::new(RefCell::new(Atom{name: "f".to_owned()})));
-    constants.insert("nil".to_owned(), Rc::new(RefCell::new(Atom{name: "nil".to_owned()})));
+    constants.insert(
+        "cons".to_owned(),
+        Rc::new(RefCell::new(Atom {
+            name: "cons".to_owned(),
+        })),
+    );
+    constants.insert(
+        "t".to_owned(),
+        Rc::new(RefCell::new(Atom {
+            name: "t".to_owned(),
+        })),
+    );
+    constants.insert(
+        "f".to_owned(),
+        Rc::new(RefCell::new(Atom {
+            name: "f".to_owned(),
+        })),
+    );
+    constants.insert(
+        "nil".to_owned(),
+        Rc::new(RefCell::new(Atom {
+            name: "nil".to_owned(),
+        })),
+    );
 
     // See https://message-from-space.readthedocs.io/en/latest/message39.html
     let mut state: ExprRef = constants.get("nil").unwrap().clone();
-    let mut point = Point{ x: 0, y: 0};
+    let mut point = Point { x: 0, y: 0 };
 
     loop {
-        let click = Rc::new(RefCell::new(Ap{
-            func: Rc::new(RefCell::new(Ap{
+        let click = Rc::new(RefCell::new(Ap {
+            func: Rc::new(RefCell::new(Ap {
                 func: constants.get("cons").unwrap().clone(),
-                arg: Rc::new(RefCell::new(Atom {name: point.x.to_string(), ..Default::default()})),
+                arg: Rc::new(RefCell::new(Atom {
+                    name: point.x.to_string(),
+                    ..Default::default()
+                })),
                 _evaluated: None,
             })),
-            arg: Rc::new(RefCell::new(Atom{name: point.y.to_string(), ..Default::default()})),
+            arg: Rc::new(RefCell::new(Atom {
+                name: point.y.to_string(),
+                ..Default::default()
+            })),
             _evaluated: None,
         }));
         let (newState, images) = interact(state.clone(), click.clone(), &functions, &constants);
@@ -283,17 +333,17 @@ fn main() {
         point = request_click_from_user();
         state = newState;
     }
-// 
-// 
-// Expr evalCons(Expr a, Expr b)
-//     Expr res = Ap(Ap(cons, eval(a)), eval(b))
-//     res.Evaluated = res
-//     return res
-// 
-// number asNum(Expr n)
-//     if (n is Atom)
-//         return PARSE_NUMBER(n.Name)
-//     ERROR("not a number")
+    //
+    //
+    // Expr evalCons(Expr a, Expr b)
+    //     Expr res = Ap(Ap(cons, eval(a)), eval(b))
+    //     res.Evaluated = res
+    //     return res
+    //
+    // number asNum(Expr n)
+    //     if (n is Atom)
+    //         return PARSE_NUMBER(n.Name)
+    //     ERROR("not a number")
 }
 
 #[cfg(test)]
