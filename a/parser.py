24f8d1758fe71e2be82e19ba83daf9a7ebcf1921
A Ray <a@machinaut.com>
2020-07-19 12:26:00 -0700

    make everything iterative

--- a/parser.py
+++ a/parser.py
@@ -1,27 +1,55 @@
 #!/usr/bin/env python
-import re
+# import re
 import sys
 from dataclasses import dataclass
 from typing import Optional, Union, List, Tuple, Dict, NamedTuple
 
 # sys.setrecursionlimit(10000)
-INT_REGEX = re.compile(r"(-?\d+)")
-
-
-class Expr:
-    """ Expressions are nodes on our parse tree """
-
-    Evaluated: Optional["Expr"]
+# INT_REGEX = re.compile(r"(-?\d+)")
 
 
 @dataclass
-class Value(Expr):
+class Value:
     """ Atoms are the leaves of our tree """
 
-    Name: Optional[Union[str, int]] = None
+    Name: Optional[str] = None
+    Evaluated: Optional["Expr"] = None
 
-    def is_int(self):
-        return isinstance(self.Name, int) or INT_REGEX.fullmatch(self.Name) is not None
+
+@dataclass
+class Tree:
+    """ Applications (of a function) are the non-leaf nodes of our tree """
+
+    Left: Optional["Expr"] = None
+    Right: Optional["Expr"] = None
+    Evaluated: Optional["Expr"] = None
+
+    def flat(self) -> List[Value]:
+        """ Flatten into a preorder list of values """
+        stack: List[Tree] = [self]
+        result: List[Value] = []
+        while stack:
+            tree: Tree = stack.pop()
+            expr: Optional["Expr"]
+            for expr in [tree.Right, tree.Left]:
+                if isinstance(expr, Value):
+                    result.append(expr)
+                elif isinstance(expr, Tree):
+                    stack.append(expr)
+                else:
+                    raise ValueError(f"Invalid type in tree {type(expr)} {expr}")
+        return result
+
+
+    def __eq__(self, other):
+        """ Non recursive equality checking """
+        if type(self) == type(other):
+            return self.flat() == other.flat()
+        return False
+
+
+Expr = Union[Tree, Value]
+ExprT = (Tree, Value)
 
 
 def asNum(n: Expr) -> int:
@@ -30,36 +58,28 @@ def asNum(n: Expr) -> int:
     raise ValueError(f"not a number {n}")
 
 
-@dataclass
-class Tree(Expr):
-    """ Applications (of a function) are the non-leaf nodes of our tree """
-
-    Left: Optional[Expr] = None
-    Right: Optional[Expr] = None
+nil: Expr = Value(Name="nil")
 
 
-nil: Expr = Value("nil")
+# @dataclass
+# class Vector(Expr):
+#     Elements: Optional[List[Union[List, int]]] = None
 
+#     def car(self) -> Expr:
+#         assert isinstance(self.Elements, list), self
+#         head = self.Elements[0]
+#         if isinstance(head, int):
+#             return Value(head)
+#         if isinstance(head, list) and len(head):
+#             return Vector(head)
+#         raise ValueError(f"bad head {head}")
 
-@dataclass
-class Vector(Expr):
-    Elements: Optional[List[Union[List, int]]] = None
-
-    def car(self) -> Expr:
-        assert isinstance(self.Elements, list), self
-        head = self.Elements[0]
-        if isinstance(head, int):
-            return Value(head)
-        if isinstance(head, list) and len(head):
-            return Vector(head)
-        raise ValueError(f"bad head {head}")
-
-    def cdr(self) -> Expr:
-        assert isinstance(self.Elements, list), self
-        tail = self.Elements[1:]
-        if len(tail):
-            return Vector(tail)
-        return nil
+#     def cdr(self) -> Expr:
+#         assert isinstance(self.Elements, list), self
+#         tail = self.Elements[1:]
+#         if len(tail):
+#             return Vector(tail)
+#         return nil
 
 
 class Vect(NamedTuple):
@@ -70,9 +90,9 @@ class Vect(NamedTuple):
 
 
 # VECTOR_DETECTOR = re.compile(r"((ap\s+ap\s+cons|\d+|nil)\s+)+nil")
-cons: Expr = Value("cons")
-t: Expr = Value("t")
-f: Expr = Value("f")
+cons: Expr = Value(Name="cons")
+t: Expr = Value(Name="t")
+f: Expr = Value(Name="f")
 
 
 # def parse(expression: str) -> Expr:
@@ -82,41 +102,55 @@ f: Expr = Value("f")
 #     return expr
 
 
-def maybe_vector(expr: Expr) -> Expr:
-    """ Try to compress a vector if possible """
-    if isinstance(expr, Tree):
-        left = expr.Left
-        if isinstance(left, Tree) and left.Left == cons:  # Possible vector
-            lr = maybe_vector(left.Right)
-            right = maybe_vector(expr.Right)
-            if right == nil:
-                right_vector: List[Union[List, int]] = []  # Vector end
-            elif isinstance(right, Vector):
-                right_vector = right.Elements  # Nested
-            else:
-                return expr  # Not a valid vector
-            if isinstance(lr, Value) and lr.is_int():
-                lr_vector: List[Union[List, int]] = [asNum(lr)]  # Base case
-            elif isinstance(lr, Vector):
-                lr_vector = [lr.Elements]  # Appending
-            else:
-                return expr  # Not a valid vector
-            return Vector(lr_vector + right_vector)
-    return expr
+# def maybe_vector(expr: Expr) -> Expr:
+#     """ Try to compress a vector if possible """
+#     if isinstance(expr, Tree):
+#         left = expr.Left
+#         if isinstance(left, Tree) and left.Left == cons:  # Possible vector
+#             lr = maybe_vector(left.Right)
+#             right = maybe_vector(expr.Right)
+#             if right == nil:
+#                 right_vector: List[Union[List, int]] = []  # Vector end
+#             elif isinstance(right, Vector):
+#                 right_vector = right.Elements  # Nested
+#             else:
+#             elif isinstance(lr, Vector):
+#                 lr_vector = [lr.Elements]  # Appending
+#             else:
+#                 return expr  # Not a valid vector
+#             return Vector(lr_vector + right_vector)
+#     return expr
 
 
-def check_expr_inner(expr: Expr) -> None:
-    assert isinstance(expr, (Value, Vector, Tree)), f"type(expr) {type(expr)}"
-    if isinstance(expr, Tree):
-        check_expr(expr.Left)
-        check_expr(expr.Right)
+# def maybe_vector(orig: Expr) -> Expr:
+#     if isinstance(orig, Tree):
+#         stack: List[Tree] = []
+#         current: Tree = orig
+#         while True:
+#             if isinstance(current, Tree):
+
+
+#         expr: Expr = orig
+
+#     return orig
+
+
+# def check_expr_inner(expr: Expr) -> None:
+#     # assert isinstance(expr, (Value, Vector, Tree)), f"type(expr) {type(expr)}"
+#     assert isinstance(expr, (Value, Tree)), f"type(expr) {type(expr)}"
+#     if isinstance(expr, Tree):
+#         check_expr(expr.Left)
+#         check_expr(expr.Right)
 
 
 def check_expr(expr: Expr) -> None:
-    try:
-        check_expr_inner(expr)
-    except Exception as e:
-        raise ValueError(f"Bad {expr} \n {e}")
+    pass
+
+
+#     try:
+#         check_expr_inner(expr)
+#     except Exception as e:
+#         raise ValueError(f"Bad {expr} \n {e}")
 
 
 # def parse_tokens(tokens: List[str]) -> Tuple[Expr, List[str]]:
@@ -134,38 +168,33 @@ def check_expr(expr: Expr) -> None:
 def parse(orig: Union[str, List[str]]) -> Expr:
     """ Parse and return a complete expression, and leftover tokens """
     tokens: List[str] = orig.strip().split() if isinstance(orig, str) else orig
-    orig_str: str = ' '.join(tokens)
+    orig_str: str = " ".join(tokens)
     stack: List[Tree] = []
     expr: Optional[Expr] = None  # Contains the object to return at the end
     while tokens:
         token, *tokens = tokens
-        # print("parser step")
-        # print("stack", stack)
-        # print("token[s]", token, tokens)
-        expr = Tree() if token == "ap" else Value(token)
+        expr = Tree() if token == "ap" else Value(Name=token)
         if stack:
             if stack[-1].Left is None:
-                # print("Connecting to stack head left")
                 stack[-1].Left = expr
             elif stack[-1].Right is None:
-                # print("Connecting to stack head right")
                 stack[-1].Right = expr
                 if isinstance(expr, Tree):
-                    # print("Append Expr", expr)
                     stack.append(expr)
             else:
-                raise ValueError(f"Parser shouldn't have complete tree in stack! {orig_str}")
+                raise ValueError(
+                    f"Parser shouldn't have complete tree in stack! {orig_str}"
+                )
             while stack and stack[-1].Left is not None and stack[-1].Right is not None:
                 expr = stack.pop()
-        # print("Got expr", expr)
+                # expr = maybe_vector(expr)
         if isinstance(expr, Tree) and (expr.Left is None or expr.Right is None):
             stack.append(expr)
         if not len(stack):
-            # print("Breaking, stack", stack)
             break
 
     assert tokens == [], f"Failed to parse tokens {orig_str} -> {tokens}"
-    assert isinstance(expr, Expr), f"Failed to parse tokens {orig_str}"
+    assert isinstance(expr, (ExprT)), f"Failed to parse tokens {orig_str}"
     return expr
 
 
@@ -178,7 +207,7 @@ def parse_file(filename: str) -> Dict[str, Expr]:
     return functions
 
 
-functions: Dict[str, Expr] = {} # parse_file("galaxy.txt")
+functions: Dict[str, Expr] = parse_file("galaxy.txt")
 
 
 def evaluate(expr: Expr) -> Expr:
@@ -207,15 +236,15 @@ def tryEval(expr: Expr) -> Expr:
     ):
         return functions[expr.Name]
     if isinstance(expr, Tree):
+        assert expr.Left is not None, expr
+        assert expr.Right is not None, expr
         left: Expr = evaluate(expr.Left)
         x: Expr = expr.Right
         check_expr(left)
         check_expr(x)
-        print("left", type(left))
-        print("x", type(x))
         if isinstance(left, Value):
             if left.Name == "neg":
-                return Value(-asNum(evaluate(x)))
+                return Value(Name=str(-asNum(evaluate(x))))
             if left.Name == "i":
                 return x
             if left.Name == "nil":
@@ -223,32 +252,32 @@ def tryEval(expr: Expr) -> Expr:
             if left.Name == "isnil":
                 return Tree(x, Tree(t, Tree(t, f)))
             if left.Name == "car":
-                if isinstance(x, Vector):
-                    return x.car()
+                # if isinstance(x, Vector):
+                #     return x.car()
                 return Tree(x, t)
             if left.Name == "cdr":
-                if isinstance(x, Vector):
-                    return x.cdr()
+                # if isinstance(x, Vector):
+                #     return x.cdr()
                 return Tree(x, f)
         if isinstance(left, Tree):
+            assert left.Left is not None, left
+            assert left.Right is not None, left
             left2: Expr = evaluate(left.Left)
             y: Expr = left.Right
             check_expr(left2)
             check_expr(y)
-            print("left2", type(left2))
-            print("y", type(y))
             if isinstance(left2, Value):
                 if left2.Name == "t":
                     return y
                 if left2.Name == "f":
                     return x
                 if left2.Name == "add":
-                    return Value(asNum(evaluate(x)) + asNum(evaluate(y)))
+                    return Value(Name=str(asNum(evaluate(x)) + asNum(evaluate(y))))
                 if left2.Name == "mul":
-                    return Value(asNum(evaluate(x)) * asNum(evaluate(y)))
+                    return Value(Name=str(asNum(evaluate(x)) * asNum(evaluate(y))))
                 if left2.Name == "div":
                     a, b = asNum(evaluate(y)), asNum(evaluate(x))
-                    return Value(a // b if a * b > 0 else (a + (-a % b)) // b)
+                    return Value(Name=str(a // b if a * b > 0 else (a + (-a % b)) // b))
                 if left2.Name == "lt":
                     return t if asNum(evaluate(y)) < asNum(evaluate(x)) else f
                 if left2.Name == "eq":
@@ -256,6 +285,8 @@ def tryEval(expr: Expr) -> Expr:
                 if left2.Name == "cons":
                     return evalCons(y, x)
             if isinstance(left2, Tree):
+                assert left2.Left is not None, left2
+                assert left2.Right is not None, left2
                 left3: Expr = evaluate(left2.Left)
                 z: Expr = left2.Right
                 check_expr(left3)
@@ -268,14 +299,15 @@ def tryEval(expr: Expr) -> Expr:
                     if left3.Name == "b":
                         return Tree(z, Tree(y, x))
                     if left3.Name == "cons":
-                        return maybe_vector(Tree(Tree(x, z), y))
+                        # return maybe_vector(Tree(Tree(x, z), y))
+                        return Tree(Tree(x, z), y)
     return expr
 
 
 def evalCons(a: Expr, b: Expr) -> Expr:
     """ Evaluate a pair """
     res: Expr = Tree(Tree(cons, evaluate(a)), evaluate(b))
-    res = maybe_vector(res)
+    # res = maybe_vector(res)
     res.Evaluated = res
     return res
 
@@ -310,16 +342,16 @@ def GET_LIST_ITEMS_FROM_EXPR(res: Expr) -> Tuple[Value, Expr, Expr]:
 
 
 if __name__ == "__main__":
-    # state: Expr = nil
-    # vector: Vect = Vect(0, 0)
+    state: Expr = nil
+    vector: Vect = Vect(0, 0)
 
-    # # main loop
-    # for _ in range(1):  # while True:
-    #     click: Expr = Tree(Tree(cons, Value(vector.X)), Value(vector.Y))
-    #     newState, images = interact(state, click)
-    #     PRINT_IMAGES(images)
-    #     vector = REQUEST_CLICK_FROM_USER()
-    #     state = newState
-    print(parse("ap ap cons 7 ap ap cons 123 nil"))
-    print(parse('ap ap cons ap ap cons 0 ap ap cons ap ap :1162 14 ap neg 64 ap ap cons :1043 ap ap cons :1059 ap ap cons ap neg 1 nil ap ap cons ap ap cons 1 ap ap cons ap ap :1162 ap neg 4 94 ap ap cons :1044 ap ap cons :1060 ap ap cons 2 nil ap ap cons ap ap cons 2 ap ap cons ap ap :1162 ap neg 78 ap neg 67 ap ap cons :1045 ap ap cons :1061 ap ap cons 1 nil ap ap cons ap ap cons 3 ap ap cons ap ap :1162 ap neg 38 ap neg 46 ap ap cons :1046 ap ap cons :1062 ap ap cons ap neg 1 nil ap ap cons ap ap cons 4 ap ap cons ap ap :1162 44 ap neg 34 ap ap cons :1047 ap ap cons :1063 ap ap cons ap neg 1 nil ap ap cons ap ap cons 5 ap ap cons ap ap :1162 60 ap neg 30 ap ap cons :1048 ap ap cons :1064 ap ap cons 3 nil ap ap cons ap ap cons 6 ap ap cons ap ap :1162 ap neg 81 11 ap ap cons :1049 ap ap cons :1065 ap ap cons 0 nil ap ap cons ap ap cons 7 ap ap cons ap ap :1162 ap neg 49 34 ap ap cons :1050 ap ap cons :1066 ap ap cons ap neg 1 nil ap ap cons ap ap cons 8 ap ap cons ap ap :1162 52 27 ap ap cons :1051 ap ap cons :1067 ap ap cons ap neg 1 nil ap ap cons ap ap cons 9 ap ap cons ap ap :1162 99 15 ap ap cons :1052 ap ap cons :1068 ap ap cons ap neg 1 nil ap ap cons ap ap cons 10 ap ap cons ap ap :1162 96 35 ap ap cons :1053 ap ap cons :1069 ap ap cons ap neg 1 nil nil'))
-    parse_file('galaxy.txt')
+    # main loop
+    for _ in range(1):  # while True:
+        click: Expr = Tree(Tree(cons, Value(Name=str(vector.X))), Value(Name=str(vector.Y)))
+        newState, images = interact(state, click)
+        PRINT_IMAGES(images)
+        vector = REQUEST_CLICK_FROM_USER()
+        state = newState
+    # print(parse("ap ap cons 7 ap ap cons 123 nil").flat())
+    # print(parse('ap ap cons ap ap cons 0 ap ap cons ap ap :1162 14 ap neg 64 ap ap cons :1043 ap ap cons :1059 ap ap cons ap neg 1 nil ap ap cons ap ap cons 1 ap ap cons ap ap :1162 ap neg 4 94 ap ap cons :1044 ap ap cons :1060 ap ap cons 2 nil ap ap cons ap ap cons 2 ap ap cons ap ap :1162 ap neg 78 ap neg 67 ap ap cons :1045 ap ap cons :1061 ap ap cons 1 nil ap ap cons ap ap cons 3 ap ap cons ap ap :1162 ap neg 38 ap neg 46 ap ap cons :1046 ap ap cons :1062 ap ap cons ap neg 1 nil ap ap cons ap ap cons 4 ap ap cons ap ap :1162 44 ap neg 34 ap ap cons :1047 ap ap cons :1063 ap ap cons ap neg 1 nil ap ap cons ap ap cons 5 ap ap cons ap ap :1162 60 ap neg 30 ap ap cons :1048 ap ap cons :1064 ap ap cons 3 nil ap ap cons ap ap cons 6 ap ap cons ap ap :1162 ap neg 81 11 ap ap cons :1049 ap ap cons :1065 ap ap cons 0 nil ap ap cons ap ap cons 7 ap ap cons ap ap :1162 ap neg 49 34 ap ap cons :1050 ap ap cons :1066 ap ap cons ap neg 1 nil ap ap cons ap ap cons 8 ap ap cons ap ap :1162 52 27 ap ap cons :1051 ap ap cons :1067 ap ap cons ap neg 1 nil ap ap cons ap ap cons 9 ap ap cons ap ap :1162 99 15 ap ap cons :1052 ap ap cons :1068 ap ap cons ap neg 1 nil ap ap cons ap ap cons 10 ap ap cons ap ap :1162 96 35 ap ap cons :1053 ap ap cons :1069 ap ap cons ap neg 1 nil nil'))
+    # parse_file('galaxy.txt')
--- a/test_parser.py
+++ a/test_parser.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
 import unittest
-from parser import Tree, Value, Expr, parse, parse_file, Vect, Vector
+from parser import Tree, Value, Expr, parse, parse_file, Vect
 
 
-class TestClasses(unittest.TestCase):
-    def test_repr(self):
-        for e in [
-            Expr(),
-            Value("0"),
-            Value("nil"),
-            Tree(Expr(), Value("2")),
-            Tree(Tree(Value("4"), Value("5")), Expr()),
-            Vect(0, 0),
-            Vect(1, 2),
-        ]:
-            self.assertEqual(eval(repr(e)), e)
+# class TestClasses(unittest.TestCase):
+#     def test_repr(self):
+#         for e in [
+#             Expr(),
+#             Value("0"),
+#             Value("nil"),
+#             Tree(Expr(), Value("2")),
+#             Tree(Tree(Value("4"), Value("5")), Expr()),
+#             Vect(0, 0),
+#             Vect(1, 2),
+#         ]:
+#             self.assertEqual(eval(repr(e)), e)
 
 
 class TestParser(unittest.TestCase):
@@ -25,8 +25,8 @@ class TestParser(unittest.TestCase):
             ("nil", Value("nil")),
             ("ap inc 1", Tree(Value("inc"), Value("1"))),
             ("ap ap add 1 2", Tree(Tree(Value("add"), Value("1")), Value("2"))),
-            ("ap ap cons 0 nil", Vector([0])),
-            ("ap ap cons 1 ap ap cons 2 nil", Vector([1, 2])),
+            # ("ap ap cons 0 nil", Vector([0])),
+            # ("ap ap cons 1 ap ap cons 2 nil", Vector([1, 2])),
         ]:
             self.assertEqual(parse(s), e)
 
@@ -34,15 +34,15 @@ class TestParser(unittest.TestCase):
         parse_file("galaxy.txt")
 
 
-class TestVector(unittest.TestCase):
-    def test_parse(self):
-        for s, v in [
-            ("ap ap cons 0 nil", [0]),
-            ("ap ap cons 1 ap ap cons 0 nil", [1, 0]),
-            ("ap ap cons ap ap cons 0 nil nil", [[0]]),
-            ("ap ap cons 1 ap ap cons ap ap cons 0 nil nil", [1, [0]]),
-        ]:
-            self.assertEqual(parse(s), Vector(v))
+# class TestVector(unittest.TestCase):
+#     def test_parse(self):
+#         for s, v in [
+#             ("ap ap cons 0 nil", [0]),
+#             ("ap ap cons 1 ap ap cons 0 nil", [1, 0]),
+#             ("ap ap cons ap ap cons 0 nil nil", [[0]]),
+#             ("ap ap cons 1 ap ap cons ap ap cons 0 nil nil", [1, [0]]),
+#         ]:
+#             self.assertEqual(parse(s), Vector(v))
 
 
 if __name__ == "__main__":
