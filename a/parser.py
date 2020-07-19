1666b51c7ef1a65f9891950f32594cd8b2933a23
A Ray <a@machinaut.com>
2020-07-19 11:38:03 -0700

    iterative parser

--- a/parser.py
+++ a/parser.py
@@ -4,7 +4,7 @@ import sys
 from dataclasses import dataclass
 from typing import Optional, Union, List, Tuple, Dict, NamedTuple
 
-sys.setrecursionlimit(10000)
+# sys.setrecursionlimit(10000)
 INT_REGEX = re.compile(r"(-?\d+)")
 
 
@@ -13,75 +13,40 @@ class Expr:
 
     Evaluated: Optional["Expr"]
 
-    def __init__(self, Evaluated: Optional["Expr"] = None):
-        assert Evaluated is None or isinstance(Evaluated, Expr), repr(Evaluated)
-        self.Evaluated = Evaluated
 
-    def __repr__(self):
-        fields = ",".join(
-            f"{k}={repr(v)}" for k, v in vars(self).items() if k != "Evaluated"
-        )
-        return f"{self.__class__.__name__}({fields})"
-
-    def __eq__(self, other):
-        if self is other:
-            return True
-        if type(self) != type(other):
-            return False
-        vs = vars(self)
-        vo = vars(other)
-        if vs.keys() != vo.keys():
-            return False
-        return all(vs[k] == vo[k] for k in vs.keys() if k != "Evaluated")
-
-
+@dataclass
 class Value(Expr):
     """ Atoms are the leaves of our tree """
 
-    Name: Union[str, int]
-
-    def __init__(self, Name: Union[str, int], *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        assert isinstance(Name, (str, int)), repr(Name)
-        self.Name = Name
+    Name: Optional[Union[str, int]] = None
 
     def is_int(self):
         return isinstance(self.Name, int) or INT_REGEX.fullmatch(self.Name) is not None
 
 
 def asNum(n: Expr) -> int:
-    if isinstance(n, Value):
+    if isinstance(n, Value) and n.Name is not None:
         return int(n.Name)
     raise ValueError(f"not a number {n}")
 
 
+@dataclass
 class Tree(Expr):
     """ Applications (of a function) are the non-leaf nodes of our tree """
 
-    Left: Expr
-    Right: Expr
-
-    def __init__(self, Left: Expr, Right: Expr, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        assert isinstance(Left, Expr) and isinstance(Right, Expr), repr(Left) + repr(
-            Right
-        )
-        self.Left = Left
-        self.Right = Right
+    Left: Optional[Expr] = None
+    Right: Optional[Expr] = None
 
 
 nil: Expr = Value("nil")
 
 
+@dataclass
 class Vector(Expr):
-    Elements: List[Union[List, int]]
-
-    def __init__(self, Elements: List[Union[List, int]], *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        assert isinstance(Elements, list), repr(Elements)
-        self.Elements = Elements
+    Elements: Optional[List[Union[List, int]]] = None
 
     def car(self) -> Expr:
+        assert isinstance(self.Elements, list), self
         head = self.Elements[0]
         if isinstance(head, int):
             return Value(head)
@@ -90,6 +55,7 @@ class Vector(Expr):
         raise ValueError(f"bad head {head}")
 
     def cdr(self) -> Expr:
+        assert isinstance(self.Elements, list), self
         tail = self.Elements[1:]
         if len(tail):
             return Vector(tail)
@@ -109,11 +75,11 @@ t: Expr = Value("t")
 f: Expr = Value("f")
 
 
-def parse(expression: str) -> Expr:
-    """ Parse a complete expression """
-    expr, tokens = parse_tokens(expression.strip().split())
-    assert tokens == [], f"Leftover tokens {expression} -> {tokens}"
-    return expr
+# def parse(expression: str) -> Expr:
+#     """ Parse a complete expression """
+#     expr, tokens = parse_tokens(expression.strip().split())
+#     assert tokens == [], f"Leftover tokens {expression} -> {tokens}"
+#     return expr
 
 
 def maybe_vector(expr: Expr) -> Expr:
@@ -144,7 +110,7 @@ def check_expr_inner(expr: Expr) -> None:
     if isinstance(expr, Tree):
         check_expr(expr.Left)
         check_expr(expr.Right)
-    
+
 
 def check_expr(expr: Expr) -> None:
     try:
@@ -153,16 +119,54 @@ def check_expr(expr: Expr) -> None:
         raise ValueError(f"Bad {expr} \n {e}")
 
 
-def parse_tokens(tokens: List[str]) -> Tuple[Expr, List[str]]:
+# def parse_tokens(tokens: List[str]) -> Tuple[Expr, List[str]]:
+#     """ Parse and return a complete expression, and leftover tokens """
+#     token, *tokens = tokens
+#     if token == "ap":
+#         left, tokens = parse_tokens(tokens)
+#         right, tokens = parse_tokens(tokens)
+#         expr = Tree(Left=left, Right=right)
+#         check_expr(expr)
+#         return maybe_vector(expr), tokens
+#     return Value(token), tokens
+
+
+def parse(orig: Union[str, List[str]]) -> Expr:
     """ Parse and return a complete expression, and leftover tokens """
-    token, *tokens = tokens
-    if token == "ap":
-        left, tokens = parse_tokens(tokens)
-        right, tokens = parse_tokens(tokens)
-        expr = Tree(Left=left, Right=right)
-        check_expr(expr)
-        return maybe_vector(expr), tokens
-    return Value(token), tokens
+    tokens: List[str] = orig.strip().split() if isinstance(orig, str) else orig
+    orig_str: str = ' '.join(tokens)
+    stack: List[Tree] = []
+    expr: Optional[Expr] = None  # Contains the object to return at the end
+    while tokens:
+        token, *tokens = tokens
+        # print("parser step")
+        # print("stack", stack)
+        # print("token[s]", token, tokens)
+        expr = Tree() if token == "ap" else Value(token)
+        if stack:
+            if stack[-1].Left is None:
+                # print("Connecting to stack head left")
+                stack[-1].Left = expr
+            elif stack[-1].Right is None:
+                # print("Connecting to stack head right")
+                stack[-1].Right = expr
+                if isinstance(expr, Tree):
+                    # print("Append Expr", expr)
+                    stack.append(expr)
+            else:
+                raise ValueError(f"Parser shouldn't have complete tree in stack! {orig_str}")
+            while stack and stack[-1].Left is not None and stack[-1].Right is not None:
+                expr = stack.pop()
+        # print("Got expr", expr)
+        if isinstance(expr, Tree) and (expr.Left is None or expr.Right is None):
+            stack.append(expr)
+        if not len(stack):
+            # print("Breaking, stack", stack)
+            break
+
+    assert tokens == [], f"Failed to parse tokens {orig_str} -> {tokens}"
+    assert isinstance(expr, Expr), f"Failed to parse tokens {orig_str}"
+    return expr
 
 
 def parse_file(filename: str) -> Dict[str, Expr]:
@@ -170,13 +174,11 @@ def parse_file(filename: str) -> Dict[str, Expr]:
     functions = {}
     for line in open(filename).readlines():
         name, _, *tokens = line.strip().split()
-        expr, leftover = parse_tokens(tokens)
-        assert leftover == [], f"Got leftover tokens {line} -> {tokens}"
-        functions[name] = expr
+        functions[name] = parse(tokens)
     return functions
 
 
-functions: Dict[str, Expr] = parse_file("galaxy.txt")
+functions: Dict[str, Expr] = {} # parse_file("galaxy.txt")
 
 
 def evaluate(expr: Expr) -> Expr:
@@ -209,8 +211,8 @@ def tryEval(expr: Expr) -> Expr:
         x: Expr = expr.Right
         check_expr(left)
         check_expr(x)
-        print('left', type(left))
-        print('x', type(x))
+        print("left", type(left))
+        print("x", type(x))
         if isinstance(left, Value):
             if left.Name == "neg":
                 return Value(-asNum(evaluate(x)))
@@ -233,8 +235,8 @@ def tryEval(expr: Expr) -> Expr:
             y: Expr = left.Right
             check_expr(left2)
             check_expr(y)
-            print('left2', type(left2))
-            print('y', type(y))
+            print("left2", type(left2))
+            print("y", type(y))
             if isinstance(left2, Value):
                 if left2.Name == "t":
                     return y
@@ -308,14 +310,16 @@ def GET_LIST_ITEMS_FROM_EXPR(res: Expr) -> Tuple[Value, Expr, Expr]:
 
 
 if __name__ == "__main__":
-    state: Expr = nil
-    vector: Vect = Vect(0, 0)
+    # state: Expr = nil
+    # vector: Vect = Vect(0, 0)
 
-    # main loop
-    for _ in range(1):  # while True:
-        click: Expr = Tree(Tree(cons, Value(vector.X)), Value(vector.Y))
-        newState, images = interact(state, click)
-        PRINT_IMAGES(images)
-        vector = REQUEST_CLICK_FROM_USER()
-        state = newState
-
+    # # main loop
+    # for _ in range(1):  # while True:
+    #     click: Expr = Tree(Tree(cons, Value(vector.X)), Value(vector.Y))
+    #     newState, images = interact(state, click)
+    #     PRINT_IMAGES(images)
+    #     vector = REQUEST_CLICK_FROM_USER()
+    #     state = newState
+    print(parse("ap ap cons 7 ap ap cons 123 nil"))
+    print(parse('ap ap cons ap ap cons 0 ap ap cons ap ap :1162 14 ap neg 64 ap ap cons :1043 ap ap cons :1059 ap ap cons ap neg 1 nil ap ap cons ap ap cons 1 ap ap cons ap ap :1162 ap neg 4 94 ap ap cons :1044 ap ap cons :1060 ap ap cons 2 nil ap ap cons ap ap cons 2 ap ap cons ap ap :1162 ap neg 78 ap neg 67 ap ap cons :1045 ap ap cons :1061 ap ap cons 1 nil ap ap cons ap ap cons 3 ap ap cons ap ap :1162 ap neg 38 ap neg 46 ap ap cons :1046 ap ap cons :1062 ap ap cons ap neg 1 nil ap ap cons ap ap cons 4 ap ap cons ap ap :1162 44 ap neg 34 ap ap cons :1047 ap ap cons :1063 ap ap cons ap neg 1 nil ap ap cons ap ap cons 5 ap ap cons ap ap :1162 60 ap neg 30 ap ap cons :1048 ap ap cons :1064 ap ap cons 3 nil ap ap cons ap ap cons 6 ap ap cons ap ap :1162 ap neg 81 11 ap ap cons :1049 ap ap cons :1065 ap ap cons 0 nil ap ap cons ap ap cons 7 ap ap cons ap ap :1162 ap neg 49 34 ap ap cons :1050 ap ap cons :1066 ap ap cons ap neg 1 nil ap ap cons ap ap cons 8 ap ap cons ap ap :1162 52 27 ap ap cons :1051 ap ap cons :1067 ap ap cons ap neg 1 nil ap ap cons ap ap cons 9 ap ap cons ap ap :1162 99 15 ap ap cons :1052 ap ap cons :1068 ap ap cons ap neg 1 nil ap ap cons ap ap cons 10 ap ap cons ap ap :1162 96 35 ap ap cons :1053 ap ap cons :1069 ap ap cons ap neg 1 nil nil'))
+    parse_file('galaxy.txt')
