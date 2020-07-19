ff1634e21673977c1cba80e534fcae1bebe70ab2
Alex Kesling <alex@kesling.co>
2020-07-19 18:45:30 -0400

    Add name() at Expr level to avoid dynamic trait mumbo jumbo

--- sidecar/icfpjit/src/pseudo.rs
+++ sidecar/icfpjit/src/pseudo.rs
@@ -9,6 +9,7 @@ use std::ptr;
 
 type ExprRef = Rc<RefCell<dyn Expr>>;
 trait Expr: Debug {
+    fn name(&self) -> Option<&str>;
     fn evaluated(&self) -> &Option<ExprRef>;
     fn set_evaluated(&mut self, ExprRef) -> Result<(), String>;
 }
@@ -21,6 +22,10 @@ struct Atom {
 }
 
 impl Expr for Atom {
+    fn name(&self) -> Option<&str> {
+        Some(&self.name)
+    }
+
     fn evaluated(&self) -> &Option<ExprRef> {
         return &self._evaluated;
     }
@@ -31,6 +36,10 @@ impl Expr for Atom {
 }
 
 impl Expr for &Atom {
+    fn name(&self) -> Option<&str> {
+        Some(&self.name)
+    }
+
     fn evaluated(&self) -> &Option<ExprRef> {
         return &self._evaluated;
     }
@@ -49,6 +58,10 @@ struct Ap {
 }
 
 impl Expr for Ap {
+    fn name(&self) -> Option<&str> {
+        None
+    }
+
     fn evaluated(&self) -> &Option<ExprRef> {
         return &self._evaluated;
     }
@@ -60,6 +73,10 @@ impl Expr for Ap {
 }
 
 impl Expr for &Ap {
+    fn name(&self) -> Option<&str> {
+        None
+    }
+
     fn evaluated(&self) -> &Option<ExprRef> {
         return &self._evaluated;
     }
@@ -81,7 +98,7 @@ fn parse_functions(script_string: &str) -> HashMap<String, ExprRef> {
     panic!("Parse functions is not yet implemented");
 }
 
-fn interact(state: ExprRef, event: ExprRef) -> (ExprRef, ExprRef) {
+fn interact(state: ExprRef, event: ExprRef, functions: &HashMap<String, ExprRef>) -> (ExprRef, ExprRef) {
     // See https://message-from-space.readthedocs.io/en/latest/message38.html
     let expr: ExprRef = Rc::new(RefCell::new(Ap{
         func: Some(Rc::new(RefCell::new(Ap{
@@ -92,7 +109,7 @@ fn interact(state: ExprRef, event: ExprRef) -> (ExprRef, ExprRef) {
         arg: Some(event.clone()),
         ..Default::default()
     }));
-    let res: ExprRef = eval(expr).unwrap();
+    let res: ExprRef = eval(expr, functions).unwrap();
     // Note: res will be modulatable here (consists of cons, nil and numbers only)
     let items = get_list_items_from_expr(res).unwrap();
     if items.len() < 3 {
@@ -100,9 +117,9 @@ fn interact(state: ExprRef, event: ExprRef) -> (ExprRef, ExprRef) {
     }
     let (flag, newState, data) = (items[0].clone(), items[1].clone(), items[2].clone());
     if (as_num(flag) == 0) {
-        return (newState, data)
+        return (newState, data);
     }
-    return interact(newState, send_to_alien_proxy(data))
+    return interact(newState, send_to_alien_proxy(data), functions);
 }
 
 fn send_to_alien_proxy(expr: ExprRef) -> ExprRef {
@@ -117,7 +134,7 @@ fn get_list_items_from_expr(expr: ExprRef) -> Result<Vec<ExprRef>, String> {
     panic!("Eval is not yet implemented");
 }
 
-fn eval(expr: ExprRef) -> Result<ExprRef, String> {
+fn eval(expr: ExprRef, functions: &HashMap<String, ExprRef>) -> Result<ExprRef, String> {
     match expr.borrow().evaluated() {
         Some(expr) => {
             panic!("Eval is not yet implemented");
@@ -125,7 +142,7 @@ fn eval(expr: ExprRef) -> Result<ExprRef, String> {
         None => {
             let mut current_expr = expr.clone();
             loop {
-                let result = try_eval(current_expr.clone())?;
+                let result = try_eval(current_expr.clone(), functions)?;
                 if ptr::eq(current_expr.as_ref(), result.as_ref()) {
                     current_expr.borrow_mut().set_evaluated(result.clone())?;
                     return Ok(result);
@@ -137,8 +154,51 @@ fn eval(expr: ExprRef) -> Result<ExprRef, String> {
     }
 }
 
-fn try_eval(expr: ExprRef) -> Result<ExprRef, String> {
-    panic!("try_eval is not yet implemented");
+fn try_eval(expr: ExprRef, functions: &HashMap<String, ExprRef>) -> Result<ExprRef, String> {
+    match expr.borrow().evaluated() {
+        Some(x) => {
+            return Ok(x.clone());
+        }
+        None => {
+            if let Some(name) = expr.borrow().name().as_ref() {
+                if let Some(function) = functions.get(name.to_owned()) {
+                    return Ok(function.clone());
+                }
+            }
+            panic!("try_eval is not yet implemented");
+        //    if (expr is Ap)
+        //        Expr fun = eval(expr.Fun)
+        //        Expr x = expr.Arg
+        //        if (fun is Atom)
+        //            if (fun.Name == "neg") return Atom(-asNum(eval(x)))
+        //            if (fun.Name == "i") return x
+        //            if (fun.Name == "nil") return t
+        //            if (fun.Name == "isnil") return Ap(x, Ap(t, Ap(t, f)))
+        //            if (fun.Name == "car") return Ap(x, t)
+        //            if (fun.Name == "cdr") return Ap(x, f)
+        //        if (fun is Ap)
+        //            Expr fun2 = eval(fun.Fun)
+        //            Expr y = fun.Arg
+        //            if (fun2 is Atom)
+        //                if (fun2.Name == "t") return y
+        //                if (fun2.Name == "f") return x
+        //                if (fun2.Name == "add") return Atom(asNum(eval(x)) + asNum(eval(y)))
+        //                if (fun2.Name == "mul") return Atom(asNum(eval(x)) * asNum(eval(y)))
+        //                if (fun2.Name == "div") return Atom(asNum(eval(y)) / asNum(eval(x)))
+        //                if (fun2.Name == "lt") return asNum(eval(y)) < asNum(eval(x)) ? t : f
+        //                if (fun2.Name == "eq") return asNum(eval(x)) == asNum(eval(y)) ? t : f
+        //                if (fun2.Name == "cons") return evalCons(y, x)
+        //            if (fun2 is Ap)
+        //                Expr fun3 = eval(fun2.Fun)
+        //                Expr z = fun2.Arg
+        //                if (fun3 is Atom)
+        //                    if (fun3.Name == "s") return Ap(Ap(z, x), Ap(y, x))
+        //                    if (fun3.Name == "c") return Ap(Ap(z, x), y)
+        //                    if (fun3.Name == "b") return Ap(z, Ap(y, x))
+        //                    if (fun3.Name == "cons") return Ap(Ap(x, z), y)
+        //    return expr
+        }
+    }
 }
 
 fn print_images(points: ExprRef) {
@@ -171,48 +231,11 @@ fn main() {
             arg: Some(Rc::new(RefCell::new(Atom{name: point.y.to_string(), ..Default::default()}))),
             ..Default::default()
         }));
-        let (newState, images) = interact(state.clone(), click.clone());
+        let (newState, images) = interact(state.clone(), click.clone(), &functions);
         print_images(images);
         point = request_click_from_user();
         state = newState;
     }
-// 
-// Expr tryEval(Expr expr)
-//     if (expr.Evaluated != null)
-//         return expr.Evaluated
-//     if (expr is Atom && functions[expr.Name] != null)
-//         return functions[expr.Name]
-//     if (expr is Ap)
-//         Expr fun = eval(expr.Fun)
-//         Expr x = expr.Arg
-//         if (fun is Atom)
-//             if (fun.Name == "neg") return Atom(-asNum(eval(x)))
-//             if (fun.Name == "i") return x
-//             if (fun.Name == "nil") return t
-//             if (fun.Name == "isnil") return Ap(x, Ap(t, Ap(t, f)))
-//             if (fun.Name == "car") return Ap(x, t)
-//             if (fun.Name == "cdr") return Ap(x, f)
-//         if (fun is Ap)
-//             Expr fun2 = eval(fun.Fun)
-//             Expr y = fun.Arg
-//             if (fun2 is Atom)
-//                 if (fun2.Name == "t") return y
-//                 if (fun2.Name == "f") return x
-//                 if (fun2.Name == "add") return Atom(asNum(eval(x)) + asNum(eval(y)))
-//                 if (fun2.Name == "mul") return Atom(asNum(eval(x)) * asNum(eval(y)))
-//                 if (fun2.Name == "div") return Atom(asNum(eval(y)) / asNum(eval(x)))
-//                 if (fun2.Name == "lt") return asNum(eval(y)) < asNum(eval(x)) ? t : f
-//                 if (fun2.Name == "eq") return asNum(eval(x)) == asNum(eval(y)) ? t : f
-//                 if (fun2.Name == "cons") return evalCons(y, x)
-//             if (fun2 is Ap)
-//                 Expr fun3 = eval(fun2.Fun)
-//                 Expr z = fun2.Arg
-//                 if (fun3 is Atom)
-//                     if (fun3.Name == "s") return Ap(Ap(z, x), Ap(y, x))
-//                     if (fun3.Name == "c") return Ap(Ap(z, x), y)
-//                     if (fun3.Name == "b") return Ap(z, Ap(y, x))
-//                     if (fun3.Name == "cons") return Ap(Ap(x, z), y)
-//     return expr
 // 
 // 
 // Expr evalCons(Expr a, Expr b)
