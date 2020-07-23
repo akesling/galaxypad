66d50f7d4095d295beedf51a45458420e1830c7c
Alex Kesling <alex@kesling.co>
2020-07-23 00:06:15 -0400

    Get initial wasm-bindgen / wasm-pack working
    There are still issues with stack size when loading galaxy.txt :/

--- galaxypad/host/src/lib.rs
+++ galaxypad/host/src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use runtime::{entry_point, Point};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -10,10 +11,30 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, host!");
+    log("Hello, host!");
+}
+
+fn print_names(points: Vec<Vec<(i64, i64)>>) {
+    log(&format!("{:?}", points));
+}
+
+fn click_panic() -> Point {
+    panic!("Click receipt is not yet implemented.");
+}
+
+#[wasm_bindgen]
+pub fn test_stack_size(stack_depth: i32) {
+    log(&format!("{}", stack_depth));
+    test_stack_size(stack_depth + 1);
+}
+
+#[wasm_bindgen]
+pub fn start_galaxy_pad() {
+    let callback = entry_point(&click_panic, &print_names);
 }
